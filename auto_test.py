a52591c2e2fc1b39a0375ca6bf5d071c3c00458e
Frogern <roma.morshakov@gmail.com>
2023-06-06 16:34:08 +0300

    visual changes

--- auto_test.py
+++ auto_test.py
@@ -15,15 +15,14 @@ produkty = {
 
 
 def auto(_id,_url):
-    dead_end = f'/api/v1/kits/{_id}/products'
-    respons = r.post(url=_url+dead_end, json=produkty )
+    end_point = f'/api/v1/kits/{_id}/products'
+    respons = r.post(url=_url+end_point, json=produkty )
     return respons
 
 
 def test():
     if auto(13,'https://284b3fc3-811b-40f7-a5b4-8f3cb6749ad0.serverhub.praktikum-services.ru',).status_code == 200:
-        print('Done')
-    else:
-        print('Faild')
+        return 'Done'
+    return 'Faild'
     
-test()
+print(test())
